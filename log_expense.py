23483b67bc228961dd48d8c8323f610df8b71589
DATA SCIENTIST <data@ip-10-0-20-170.us-west-2.compute.internal>
2019-08-05 10:18:16 -0500

    tested and changed logic for next payment method

--- log_expense.py
+++ log_expense.py
@@ -209,7 +209,7 @@ def list_credit_cards(credit_cards_list):
     return ccs_str
 
 def get_next_pay_date(payment_date, cut_day):
-    next_pay_day = min(ceil(cut_day/15),2)*15
+    next_pay_day = min(ceil(cut_day/15)*15, 31)
     next_pay_date = date(
         day=next_pay_day, 
         month=payment_date.month,
